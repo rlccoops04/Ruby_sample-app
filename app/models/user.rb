42843ec207f9761edcdfa6e3ae3bcf88bda740ad
Bulat <@LAPTOP-SSS2TI2M>
2023-12-07 14:34:37 +0300

    Finish user edit, update, index, and destroy actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,8 @@ class User < ApplicationRecord
                       format: { with: VALID_EMAIL_REGEX },
                       uniqueness: { case_sensitive: false }
     has_secure_password
-    validates :password, presence: true, length: { minimum: 6 }
-      # Возвращает дайджест данной строки
+    validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+    # Возвращает дайджест данной строки
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -26,8 +26,8 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     end
     test "login with remembering" do
         log_in_as(@user, remember_me: '1')
-        assert_not_nil cookies['remember_token']
-      end
+        assert_equal FILL_IN, assigns(:user).FILL_IN
+    end
     
       test "login without remembering" do
         log_in_as(@user, remember_me: '0')
